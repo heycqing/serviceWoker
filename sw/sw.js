e5b759727c98a582f242a5353d28f8f65242a740
cqing <SiqingIII@163.com>
2018-07-20 15:09:30 +0800

    添加

---
+++ sw/sw.js
@@ -0,0 +1,31 @@
+"use strict"
+if(navigator.serviceWorker){
+    window.addEventListener('load',function(){
+        // 注册service Woker事件；
+        navigator.serviceWorker.register('./sw.js').then(registeration => {
+            console.log('执行注册事件！')
+           
+        }).catch(error => {
+            console.log('错误：',error);
+        })
+    })
+   
+}
+
+const CACHE_NAME = 'service cache by cqing';
+const urlToCache =[
+    '/',
+    '/js/main.js',
+    '/css/main.css',
+    '/img/test.png'
+]
+
+// 安装事件
+self.addEventListener('install',function(event){
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache){
+            return cache.addAll(urlToCache)
+        })
+    )
+})
+
